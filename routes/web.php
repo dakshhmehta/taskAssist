21ec6cd4768600aedee500f9b74720b634489db3
Daksh Mehta <dakshhmehta@gmail.com>
2024-11-20 17:14:48 +0530

    WIP: WP Plugin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\WeeklyPlanController;
+use App\Http\Controllers\WordPressDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,5 +13,7 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('standup', [WeeklyPlanController::class, 'getStandupsheet']);
     Route::get('weekly-plan', [WeeklyPlanController::class, 'get']);
 });
+
+Route::get('wp-data', [WordPressDataController::class, 'getData']);
 
 Route::get('invoices/{id}/print', [InvoicesController::class, 'getPrint'])->name('invoices.print');
---
+++ wp-plugin/taskassist.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: Custom API for Site Info
+Description: Exposes PHP version, WordPress version, and plugin list via a custom REST API endpoint.
+Version: 1.0
+Author: Your Name
+*/
+
+function custom_api_register_routes() {
+    register_rest_route('taskassist/v1', '/site-info', [
+        'methods' => 'GET',
+        'callback' => 'custom_api_get_site_info',
+        'permission_callback' => '__return_true',
+    ]);
+}
+
+function custom_api_get_site_info() {
+    // Get PHP version and WordPress version
+    $php_version = phpversion();
+    $wp_version = get_bloginfo('version');
+
+    if($_GET['secret'] !== 'Daxdaxdax2910'){
+        exit();
+    }
+
+    // Get list of installed plugins
+    if (!function_exists('get_plugins')) {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+    $all_plugins = get_plugins();
+    $plugin_list = [];
+
+    foreach ($all_plugins as $plugin_file => $plugin_data) {
+        $plugin_list[] = [
+            'name' => $plugin_data['Name'],
+            'version' => $plugin_data['Version'],
+            'active' => is_plugin_active($plugin_file),
+        ];
+    }
+
+    // Prepare response data
+    $data = [
+        'php_version' => $php_version,
+        'wp_version' => $wp_version,
+        'plugins' => $plugin_list,
+    ];
+
+    return new WP_REST_Response($data, 200);
+}
+
+add_action('rest_api_init', 'custom_api_register_routes');
