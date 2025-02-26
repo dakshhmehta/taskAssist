29a6db82cc5ff5b97c2d95a99a65b7bb2a66228f
Daksh Mehta <dakshhmehta@gmail.com>
2025-02-26 16:13:21 +0530

    DAta collector updated

--- app/Http/Controllers/WordPressDataController.php
+++ app/Http/Controllers/WordPressDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Site;
 use Illuminate\Http\Request;
 
 class WordPressDataController extends Controller
@@ -9,9 +10,25 @@ class WordPressDataController extends Controller
     public function receiveData(Request $request)
     {
         \Log::debug($request->all());
+        $data = $request->all();
+
+
+
+        $site = Site::where('domain', $data['url'])->first();
+
+        if (! $site) {
+            $site = Site::create([
+                'domain' => $data['url'],
+            ]);
+        }
+
+        foreach ($data as $key => $value) {
+            $site->setMeta($key, $value);
+        }
     }
 
-    public function getPluginInfo(Request $request){
+    public function getPluginInfo(Request $request)
+    {
         $data = config('wp_plugin');
 
         return $data;
