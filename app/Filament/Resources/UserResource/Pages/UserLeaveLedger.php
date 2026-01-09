56ed1aaaf71a6baf4bb693df653bc502ad3242d2
Daksh Mehta <dakshhmehta@gmail.com>
2026-01-09 11:05:45 +0530

    Adjustment leave form'

--- app/Filament/Resources/UserResource/Pages/UserLeaveLedger.php
+++ app/Filament/Resources/UserResource/Pages/UserLeaveLedger.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use App\Filament\Resources\UserResource\Widgets\UserCLBalance;
 use App\Filament\Resources\UserResource\Widgets\UserLeaveLedgerTable;
+use Carbon\Carbon;
+use Filament\Actions\Action;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Pages\Page;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\Auth;
 
 class UserLeaveLedger extends ViewRecord
 {
@@ -21,6 +27,41 @@ class UserLeaveLedger extends ViewRecord
     public function mount(int | string $record): void
     {
         $this->record = $this->resolveRecord($record);
+    }
+
+    public function getHeaderActions(): array
+    {
+        return [
+            Action::make('adjust_balance')
+                ->label('Adjust Leave Balance')
+                ->form([
+                    DatePicker::make('date')
+                        ->required()
+                        ->default(now())
+                        ->maxDate(now()),
+                    TextInput::make('amount')
+                        ->label('Adjust Balance')
+                        ->numeric()
+                        ->required()
+                        ->helperText('Enter negative value to deduct leaves (e.g., -1)'),
+                    Textarea::make('description')
+                        ->required()
+                        ->label('Description'),
+                ])
+                ->action(function (array $data): void {
+                    $amount = (float) $data['amount'];
+
+                    $date = Carbon::parse($data['date']);
+                    
+                    if ($amount > 0) {
+                        $txn = $this->record->credit($amount, $date, $data['description']);
+                    } else {
+                        $txn = $this->record->debit(abs($amount), $date, $data['description']);
+                    }
+
+                    $txn->changeType('cl');
+                })
+        ];
     }
 
     public function getTitle(): string|Htmlable
