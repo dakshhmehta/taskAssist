b4cd05f8f338c9726575cbae28bf67a303b4d94f
Daksh Mehta <dakshhmehta@gmail.com>
2026-02-12 16:01:22 +0530

    Form modified for price

--- app/Filament/Resources/HostingPackageResource.php
+++ app/Filament/Resources/HostingPackageResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\HostingPackageResource\Pages;
 use App\Models\HostingPackage;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -22,7 +23,20 @@ class HostingPackageResource extends Resource
     {
         return $form
             ->schema([
-                //
+                // Add Storage
+                TextInput::make('storage')
+                    ->label('Storage')
+                    ->numeric()
+                    ->required(),
+                // Add Emails
+                TextInput::make('emails')
+                    ->label('Emails')
+                    ->required(),
+                // Add Price
+                TextInput::make('price')
+                    ->label('Price')
+                    ->numeric()
+                    ->required(),
             ]);
     }
 
